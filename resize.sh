149610b1e9903d5b7b40607ffae00822d279ea08
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2022-01-07 14:00:15 +0100

    resize.sh

--- resize.sh
+++ resize.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+## Program utilties
+
+RED="$(printf '\033[31m')"
+exit_on_signal_SIGINT() {
+    { printf "\n\n%s\n\n" "${RED} Program Interrupted." 2>&1; reset_color; }
+    exit 0
+}
+exit_on_signal_SIGTERM() {
+    { printf "\n\n%s\n\n" "${RED} Program Terminated." 2>&1; reset_color; }
+    exit 0
+}
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+reset_color() {
+	tput sgr0   # reset attributes
+	tput op     # reset color
+    return
+}
+
+## main logic
 read -p "Size to resize to: " SIZE
 
 EC2_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
@@ -7,16 +28,16 @@ EC2_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 VOLUME_ID=$(aws ec2 describe-instances --output text --instance-id $EC2_INSTANCE_ID \
   --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId")
   
-
 aws ec2 modify-volume --volume-id $VOLUME_ID --size ${SIZE}
 
-STATE=$(aws ec2 describe-volumes-modifications --volume-id $VOLUME_ID --output text | awk '{print $3}')
-
-    if [[ "$STATE" == "optimizing" ]]; then
-        echo -n "Waiting for resize operation to complete....."
-        echo " "
-        sleep 5
-    fi
+while [ \
+  "$(aws ec2 describe-volumes-modifications \
+    --volume-id $VOLUMEID \
+    --filters Name=modification-state,Values="optimizing","completed" \
+    --query "length(VolumesModifications)"\
+    --output text)" != "1" ]; do
+sleep 1
+done
     
 if [[ $(readlink -f /dev/xvda) = "/dev/xvda" ]]; then
     sudo growpart /dev/xvda 1
@@ -28,8 +49,7 @@ if [[ $(readlink -f /dev/xvda) = "/dev/xvda" ]]; then
         sudo xfs_growfs -d /
     else
         sudo resize2fs /dev/xvda1
-    fi
-    
+    fi    
 else
   sudo growpart /dev/nvme0n1 1
 
@@ -43,18 +63,3 @@ else
   fi
 fi
     
-
-
-## Script termination
-exit_on_signal_SIGINT() {
-    { printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
-    exit 0
-}
-
-exit_on_signal_SIGTERM() {
-    { printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Terminated." 2>&1; reset_color; }
-    exit 0
-}
-
-trap exit_on_signal_SIGINT SIGINT
-trap exit_on_signal_SIGTERM SIGTERM
